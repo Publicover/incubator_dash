051d806f9c51815492fbea6ec523ce111bc69032
Jim Publicover <jameslpublicover@gmail.com>
2017-05-27 16:32:02 -0400

    cut out a bunch of leftover assignment associations

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   def index
     # @students = Admin.where(id: session[:user_id]) || @students = Student.where(id: session[:user_id])
     @students = Student.where(id: session[:user_id])
-    @assignments = Assignment.where(student_id: session[:user_id])
+    # @assignments = Assignment.where(student_id: session[:user_id])
 
     # respond_to do |format|
     #   format.html
@@ -70,6 +70,6 @@ class StudentsController < ApplicationController
     end
 
     def student_params
-      params.require(:student).permit(:name, :email, :password, :flavor, :admin_id, assignment_ids: [])
+      params.require(:student).permit(:name, :email, :password, :flavor, :admin_id)
     end
 end

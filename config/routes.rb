9c084bc2edf339d7d5cfc114b30346367e490c24
Jim Publicover <jameslpublicover@gmail.com>
2017-05-27 16:00:53 -0400

    students model controller and views set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,27 @@
 Rails.application.routes.draw do
 
+  get 'students/index'
+
+  get 'students/show'
+
+  get 'students/new'
+
+  get 'students/edit'
+
+  get 'students/create'
+
+  get 'students/update'
+
+  get 'students/destroy'
+
+  root 'dashboards#index'
+
   # get 'dashboards/index'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
   resources :admins
-
-  root 'dashboards#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527180609) do
+ActiveRecord::Schema.define(version: 20170527195412) do
 
   create_table "admins", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "flavor"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "students", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"

7f985c71ed5fee17596fdabc3882da1bc0dcc349
Jim Publicover <jameslpublicover@gmail.com>
2017-05-27 16:11:34 -0400

    resources :students

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'students/index'
-
-  get 'students/show'
-
-  get 'students/new'
-
-  get 'students/edit'
-
-  get 'students/create'
-
-  get 'students/update'
-
-  get 'students/destroy'
-
-  root 'dashboards#index'
-
   # get 'dashboards/index'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
   resources :admins
+
+  resources :students
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

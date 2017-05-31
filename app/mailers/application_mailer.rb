db904dc9cae5882641a7e865f1b8e36b83703038
Jim Publicover <jameslpublicover@gmail.com>
2017-05-31 17:06:56 -0400

    clean a few things in the mailer up. next is creating another controller to see what happens.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default sender: 'jim@sprintwithdash.com'
+  # default from: 'Tester@testerson.com'
   layout 'mailer'
 end

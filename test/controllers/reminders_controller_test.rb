80d8055ea80fed843f489277e627da9a830d42ea
Jim Publicover <jameslpublicover@gmail.com>
2017-06-02 13:37:38 -0400

    worry about controller test for mailer later

---
+++ test/controllers/reminders_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RemindersControllerTest < ActionDispatch::IntegrationTest
+  # test "should get write_email" do
+  #   get reminders_write_email_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get send_email" do
+  #   message.to = "example@example.com"
+  #   post reminders_send_email_url
+  #   assert_response :success
+  # end
+
+end

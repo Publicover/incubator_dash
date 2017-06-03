84e35d179736496c3083dbf6af291b614505e8be
Jim Publicover <jameslpublicover@gmail.com>
2017-06-03 15:14:02 -0400

    misspell queue_adapter in config/application

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module IncubatorDash
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.active_job.que_adapter = :delayed_job
+    config.active_job.queue_adapter= :delayed_job
     config.assets.initialize_on_precompile = false
   end
 end

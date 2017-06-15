d96a9187d3a0ea459ccb5c468de16cc5c850a4fd
Jim Publicover <jameslpublicover@gmail.com>
2017-06-15 18:39:32 -0400

    finally! got file upload test working

--- spec/features/students_can_have_avatars_spec.rb
+++ spec/features/students_can_have_avatars_spec.rb
@@ -19,9 +19,17 @@ RSpec.describe "Student avatar" do
     # follow_redirect!
     click_link "EDIT"
     # click_button("student_avatar")
-    attach_file("student[avatar]", Rails.root + "spec/support/jennifer_connelly.jpg")
+    # , Rails.root.join('spec', 'fixtures', 'sample_photo.jpg') # Put your sample_photo.jpg at /spec/fixtures/sample_photo.jpg
+    attach_file("student[avatar]", Rails.root.join('spec', 'fixtures', 'jennifer_connelly.jpg'))
     # post student_path(id: 1), avatar: fixture_file_upload("spec/support/jennifer_connelly.jpg", "image/jpg")
-    # expect(@student.avatar).to eq "jennifer_connelly.jpg"
+    # expect(@student.avatar_file_name).to eq "jennifer_connelly.jpg"
+    # expect(@student.avatar_content_type). to eq "jpg"
+    # expect(@student.name).to eq "jill"
+    # expect(@student.name).to eq "jill"
+    click_button "Update Student"
+    # expect(page).to have_content "alt=Jennifer connelly"
+    # expect(page).to have_xpath('//img[src="//Users/jimpublicover/Desktop/incubator_dash/spec/fixtures/jennifer_connelly.jpg"]')
+    expect(page).to have_xpath("//img[contains(@src,'jennifer_connelly.jpg')]")
   end
 
 end

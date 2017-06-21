3169f5e82b74aef570a0ce5f74a7929f034347c0
Jim Publicover <jameslpublicover@gmail.com>
2017-06-20 22:26:30 -0400

    halfway to getting file type icons up in show pages

---
+++ spec/requests/students_can_change_avatar_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Student avatar" do
+
+  before do
+    new_student = Student.create(name: "jill", email: "jill@jill.com",
+      password: "password", flavor: "student", admin_id: 1)
+    new_admin = Admin.create(name: "jim", email: "jim@jim.com",
+      password: "password", flavor: "admin")
+    new_assignment = Assignment.create(title: "Bio", description: "Write a bio",
+      due_date: Date.new(2017-1-1))
+    new_homework = Homework.create(title: "cool file", student_id: 1,
+      assignment_id: 1)
+    visit '/'
+    expect(page).to have_text("You must log in to access this page")
+    fill_in 'Email', with: 'jill@jill.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Save changes'
+    expect(page).to have_text("You have logged in.")
+  end
+
+  it "allows updates" do
+    click_link "AVATAR"
+    expect(page).to have_text("Students#change_avatar")
+    attach_file("student[avatar]", Rails.root.join('spec', 'fixtures',
+      'jennifer_connelly_2.jpg'))
+    click_button "Update Student"
+    expect(page).to have_text("Students#show")
+    expect(page).to have_xpath("//img[contains(@src,'jennifer_connelly_2.jpg')]")
+  end
+
+end

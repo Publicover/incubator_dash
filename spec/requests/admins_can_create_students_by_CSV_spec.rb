afe330f8ade3e79e33374daa412b1c1d592e0c1f
Jim Publicover <jameslpublicover@gmail.com>
2017-06-26 22:01:42 -0400

    ready to start with Remember Me button

---
+++ spec/requests/admins_can_create_students_by_CSV_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Admin student creation" do
+
+  before do
+    @admin = Admin.create(name: "jim", email: "jim@jim.com", password: "password", flavor: "admin")
+    visit '/'
+    expect(page).to have_text("You must log in to access this page")
+    fill_in 'email', with: 'jim@jim.com'
+    fill_in 'password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_text("You have logged in.")
+  end
+
+  it "takes a CSV" do
+    flunk
+  end
+
+end
---
+++ spec/requests/students_can_reset_password_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Student password" do
+
+  before do
+    new_student = Student.create(name: "jill", email: "jill@jill.com",
+      password: "password", flavor: "student", admin_id: 1)
+    new_admin = Admin.create(name: "jim", email: "jim@jim.com",
+      password: "password", flavor: "admin")
+    visit '/'
+    expect(page).to have_text("You must log in to access this page")
+    fill_in 'email', with: 'jill@jill.com'
+    fill_in 'password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_text("You have logged in.")
+  end
+
+  it "sends a reset email" do
+    flunk
+  end
+
+end

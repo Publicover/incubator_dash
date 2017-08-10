f2e9ea4d40e9d37518894ab17d785e1e33a46b01
Jim Publicover <jameslpublicover@gmail.com>
2017-08-10 18:24:18 -0400

    ready to try single user table

--- ruby_scratch.rb
+++ ruby_scratch.rb
@@ -7,18 +7,3 @@ def truncate_description(description)
   stopping_point = description.index(/\./)
   short_description = description[0..stopping_point]
 end
-
-
-
-
-
-RESTFUL API:
-  using HTML as it was originally intended
-  get, put, post, delete
-  get should be idempotent: issuing the command twice should not change the outcome for either
-
-what brought you to validic?
-why do you like it there?
-how big are the teams?
-what would be my first assignment?
-what does the ideal candidate look like?
